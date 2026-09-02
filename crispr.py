6d42c4ddf739f498207a99042aae1efde5037280
Ishaan <ishchhina@gmail.com>
2026-09-02 15:16:57 +0100

    mutation started

---
+++ crispr.py
@@ -0,0 +1,13 @@
+import expression_tree as eqn
+import random
+
+equation = eqn.Expression.random(6)
+#print(str(equation))
+
+def delete_node(expression, node):
+
+
+
+    return "newexpression"
+
+#delete_node(equation, "x")
